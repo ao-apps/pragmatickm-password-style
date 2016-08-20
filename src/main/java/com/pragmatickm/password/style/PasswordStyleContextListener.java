808c37b4f18b260f14b6f4924dcfa1a5d6a80df8
AO Industries, Inc. <support@aoindustries.com>
2016-08-20 16:01:56 -0500

    Moving styles to separate package

---
+++ src/main/java/com/pragmatickm/password/style/PasswordStyleContextListener.java
@@ -0,0 +1,47 @@
+/*
+ * pragmatickm-password-style - Default style for passwords nested within SemanticCMS pages and elements.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of pragmatickm-password-style.
+ *
+ * pragmatickm-password-style is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pragmatickm-password-style is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pragmatickm-password-style.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.pragmatickm.password.style;
+
+import com.pragmatickm.password.model.Password;
+import com.semanticcms.core.servlet.SemanticCMS;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("Registers the styles for passwords in SemanticCMS.")
+public class PasswordStyleContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
+		// Add our CSS file
+		semanticCMS.addCssLink("/pragmatickm-password-style/styles.css");
+		// Add link CSS class
+		semanticCMS.addLinkCssClass(Password.class, "pragmatickm-password-password-link");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Do nothing
+	}
+}
