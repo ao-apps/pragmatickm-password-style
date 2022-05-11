46d1d75270e11eb4b4ce4627ecb1906c8709ff1e
AO Industries, Inc <support@aoindustries.com>
2022-05-11 00:20:42 -0500

    Performed manual checkstyle clean-up

--- src/main/java/com/pragmatickm/password/style/PasswordStyle.java
+++ src/main/java/com/pragmatickm/password/style/PasswordStyle.java
@@ -34,6 +34,9 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+/**
+ * Registers the styles for passwords in {@link RegistryEE} and {@link HtmlRenderer}.
+ */
 @WebListener("Registers the styles for passwords in RegistryEE and HtmlRenderer.")
 public class PasswordStyle implements ServletContextListener {
 
@@ -50,8 +53,8 @@ public class PasswordStyle implements ServletContextListener {
     RegistryEE.Application.get(servletContext)
         .activate(RESOURCE_GROUP)// TODO: Activate as-needed
         .getGroup(RESOURCE_GROUP)
-        .styles
-        .add(PRAGMATICKM_PASSWORD);
+            .styles
+            .add(PRAGMATICKM_PASSWORD);
 
     HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
     // Add link CSS class
